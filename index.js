b9c0980af88fdbf0c77aef13946339249d7a1b7b
Sanjana Raj <sanjana.raj.25@dartmouth.edu>
2024-06-26 10:22:05 -0400

    run example

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 #!/usr/bin/env node
 
+import { exec } from 'child_process';
+
 const url = process.argv.slice(2)
+const file = 'example.ts'
 console.log(`url: ${url}`);
-console.log("testing npx! this worked!");
+
+exec(`npx ts-node ${file} ${url}`, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error executing ${file}: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.error(`Error output from ${file}: ${stderr}`);
+      return;
+    }
+    console.log("script run, output produced")
+  });
+
+
 
