f8bfd374bb1094bf8c395b9e01ba00f8ab30c021
Sanjana Raj <sanjana.raj.25@dartmouth.edu>
2024-06-26 11:19:25 -0400

    a

--- index.mjs
+++ index.mjs
@@ -1,22 +1,32 @@
 #!/usr/bin/env node
 
-import { exec } from 'child_process';
+// import { exec } from 'child_process';
 
 const url = process.argv.slice(2)
 const file = 'example.ts'
 console.log(`url: ${url}`);
 
-exec(`npm run example ${url}`, (error, stdout, stderr) => {
-    if (error) {
-      console.error(`Error executing ${file}: ${error.message}`);
-      return;
-    }
-    if (stderr) {
-      console.error(`Error output from ${file}: ${stderr}`);
-      return;
-    }
-    console.log("script run, output produced")
-  });
+// OR...
+const { exec, spawn } = require('node:child_process');
+exec(file, (err, stdout, stderr) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(stdout);
+});
+
+// exec(`npm run example ${url}`, (error, stdout, stderr) => {
+//     if (error) {
+//       console.error(`Error executing ${file}: ${error.message}`);
+//       return;
+//     }
+//     if (stderr) {
+//       console.error(`Error output from ${file}: ${stderr}`);
+//       return;
+//     }
+//     console.log("script run, output produced")
+//   });
 
 
 
